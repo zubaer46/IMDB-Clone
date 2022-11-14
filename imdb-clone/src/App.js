aa9c5241eb9a9da462226b9d6c1d93b28006c8f8
zubaer <yousufzubaer1622@gmail.com>
2022-11-15 00:25:01 +0600

    movie detail page added

--- imdb-clone/src/App.js
+++ imdb-clone/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import Home from './pages/Home';
 import MovieList from './components/movieList/movieList';
-//import Movie from './pages/movieDetail/movie';
+import MovieDetail from './pages/movieDetail/movieDetail';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route index element={<Home />}></Route>
-          {/* <Route path='movie/:id' element={<Movie />}></Route> */}
+          <Route path='movie/:id' element={<MovieDetail />}></Route>
           <Route path='movies/:type' element={<MovieList />}></Route>
           <Route path='/*' element={<h1>Error Page</h1>}></Route>
         </Routes>
